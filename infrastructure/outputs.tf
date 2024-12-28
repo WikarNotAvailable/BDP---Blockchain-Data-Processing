0e9b6fe1da91c9d60ca0068ed651c9b76217bf09
Jakub Wrona <jakubwrona31@gmail.com>
2024-12-28 21:57:37 +0100

    comments removal

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -7,9 +7,3 @@ output "orphan_files_removal_job_name" {
   description = "Orphan files removal job name"
   value       = module.glue_jobs.orphan_files_removal_job_name
 }
-
-#Glue data catalog is dynamically created during ETL process
-/*output "bdp_database_name" {
-  description = "Glue Catalog database name"
-  value       = module.glue_catalog.database_name
-}*/
